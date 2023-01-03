6503a910c7bc0e92dfa359367377c281657dce96
Valeriy Kamenskikh <86181396+neon4on@users.noreply.github.com>
2023-01-03 13:00:58 +0400

    Add files via upload

---
+++ Fractal/Class.cpp
@@ -0,0 +1,27 @@
+﻿#include <iostream>
+#include <conio.h>
+using namespace std;
+
+int function(long int N)
+{
+	if (N == 0)
+	{
+		return 0;
+	}
+
+	if (N == 1)
+	{
+		return 1;
+	}
+
+	return N * function(N - 1);
+}
+
+int main()
+{
+	setlocale(LC_ALL, "rus");
+	long int a; cout << "Введите число для возведение в факториал: "; cin >> a;
+	cout << "Число возведённое в факториал: " << function(a) << endl;
+	system("pause");
+	_getch();
+}
